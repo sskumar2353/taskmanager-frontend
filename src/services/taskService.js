b396e8606f85d39b237a5ebecc99ab85f6e4657f
Sai Kumar Sarika <sskumar2353@gmail.com>
2026-06-19 15:46:08 +0530

    Connect frontend to production backend

--- src/services/taskService.js
+++ src/services/taskService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/tasks";
+const API_URL = `${import.meta.env.VITE_API_URL}/api/tasks`;
 
 export const getTasks = () => {
   return axios.get(API_URL);
